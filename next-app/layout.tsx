30c8f1911b59e56a806acec78a2b9324d51eaf3f
davidbailey.eth <david@pudgypenguins.io>
2024-09-14 15:07:35 +0400

    add default files

---
+++ next-app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Providers } from "@/app/providers";
+import { fonts } from "@/app/fonts";
+import { Box } from "@chakra-ui/react";
+
+export const metadata: Metadata = {
+  title: "Aptos Institute",
+  description: "Hackathon project on Aptos",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={fonts.rubik.variable}>
+      <body>
+        <Providers>
+          <Box mx="5">{children}</Box>
+        </Providers>
+      </body>
+    </html>
+  );
+}
